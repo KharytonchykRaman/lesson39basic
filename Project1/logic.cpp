8bd0a2568a282f56aca503ccf47abf38d5a4fd8c
Danila <ddanilsachko@gmail.com>
2024-02-12 16:33:18 +0300

    Add project files.

---
+++ Project1/logic.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool is_num_prime1(int number) {
+
+	if (number < 2)
+	{
+		return false;
+	}
+
+	for (int i = 2; i < number; i++)
+	{
+		if (number % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool is_num_prime2(int number) {
+
+	if (number < 2)
+	{
+		return false;
+	}
+
+	for (int i = 2; i < number / 2; i++)
+	{
+		if (number % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool is_num_prime3(int number) {
+
+	if (number < 2)
+	{
+		return false;
+	}
+
+	for (int i = 2; i < sqrt(number); i++)
+	{
+		if (number % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
---
+++ Project1/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+bool is_num_prime1(int number);
+bool is_num_prime2(int number);
+bool is_num_prime3(int number);
+
+int main() {
+
+	int number = 2147483647;
+
+	long long startTime1 = time(0);
+	int a = is_num_prime1(number);
+	long long finishTime1 = time(0);
+	cout << " " << finishTime1 - startTime1;
+
+	long long startTime2 = time(0);
+	a = is_num_prime2(number);
+	long long finishTime2 = time(0);
+	cout << " " << finishTime2 - startTime2;
+
+	long long startTime3 = time(0);
+	a = is_num_prime3(number);
+	long long finishTime3 = time(0);
+	cout << " " << finishTime3 - startTime3;
+
+	return 0;
+}
